fa8c3f71f8037f6d206d4868a73dd9469376b2dd
Halil Şenaydın <70847361+snydnhll@users.noreply.github.com>
2021-09-28 19:09:49 +0300

    Update and rename Halil_Ibrahim_Senaydin_200707069_BM103_Proje3_Soru1_1.m to Soru1_1.m

--- Halil_Ibrahim_Senaydin_200707069_BM103_Proje3/Halil_Ibrahim_Senaydin_200707069_BM103_Proje3_Soru1_1.m
+++
@@ -1,7 +0,0 @@
-function bool=Halil_Ibrahim_Senaydin_200707069_BM103_Proje3_Soru1_1(a,b,c)
-
-bool=0;
-
-if a<0 || b<0 || c<0 %Herhangi bir sayý sýfýrdan küçükse
-    bool=1; %1 yani True deðeri verir.
-end
---
+++ Halil_Ibrahim_Senaydin_200707069_BM103_Proje3/Soru1_1.m
@@ -0,0 +1,7 @@
+function bool=Soru1_1(a,b,c)
+
+bool=0;
+
+if a<0 || b<0 || c<0 %Herhangi bir sayı sıfırdan küçükse
+    bool=1; %1 yani True değeri verir.
+end
