0a32b722945c52b43c8e4be9defdefe8047eea7e
Halil Şenaydın <70847361+snydnhll@users.noreply.github.com>
2021-09-28 19:10:53 +0300

    Update and rename Halil_Ibrahim_Senaydin_200707069_BM103_Proje3_Soru1_4.m to Soru1_4.m

--- Halil_Ibrahim_Senaydin_200707069_BM103_Proje3/Halil_Ibrahim_Senaydin_200707069_BM103_Proje3_Soru1_4.m
+++
@@ -1,6 +0,0 @@
-function bool=Halil_Ibrahim_Senaydin_200707069_BM103_Proje3_Soru1_4(a,b,c)
-bool=0;
-
-if a==0 && b==0 && c==0 %Üç sayýda sýfýr ise
-    bool = 1; %True deðeri döndürür.
-end
---
+++ Halil_Ibrahim_Senaydin_200707069_BM103_Proje3/Soru1_4.m
@@ -0,0 +1,6 @@
+function bool=Proje3_Soru1_4(a,b,c)
+bool=0;
+
+if a==0 && b==0 && c==0 %Üç sayýda sýfýr ise
+    bool = 1; %True deðeri döndürür.
+end
