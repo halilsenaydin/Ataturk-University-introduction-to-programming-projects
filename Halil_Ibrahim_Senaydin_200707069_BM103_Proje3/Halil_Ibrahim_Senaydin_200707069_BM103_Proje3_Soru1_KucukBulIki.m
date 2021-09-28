6f71e607e2da9acb20dbb43403fef2f828a28bbd
Halil Şenaydın <70847361+snydnhll@users.noreply.github.com>
2021-09-28 19:13:23 +0300

    Update and rename Halil_Ibrahim_Senaydin_200707069_BM103_Proje3_Soru1_KucukBulIki.m to KucukBulIki.m

--- Halil_Ibrahim_Senaydin_200707069_BM103_Proje3/Halil_Ibrahim_Senaydin_200707069_BM103_Proje3_Soru1_KucukBulIki.m
+++
@@ -1,9 +0,0 @@
-function enKucuk = Halil_Ibrahim_Senaydin_200707069_BM103_Proje3_Soru1_KucukBulIki(sayi1,sayi2)
-
-%Ýki sayý arasýnda en büyük bulan alt fonksiyon
-
-enKucuk = sayi1;
-
-if sayi2<enKucuk
-    enKucuk = sayi2;
-end
---
+++ Halil_Ibrahim_Senaydin_200707069_BM103_Proje3/KucukBulIki.m
@@ -0,0 +1,9 @@
+function enKucuk = Soru1_KucukBulIki(sayi1,sayi2)
+
+%İki sayı arasında en büyük bulan alt fonksiyon
+
+enKucuk = sayi1;
+
+if sayi2<enKucuk
+    enKucuk = sayi2;
+end
